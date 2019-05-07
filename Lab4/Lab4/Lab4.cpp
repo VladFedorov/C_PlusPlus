44e37a4fc2757f8a6621c5585e03fcc07507b2fc
VUser <nxnxngh@gmail.ocm>
2019-05-07 21:57:29 +0300

    Done

---
+++ Lab4/Lab4/Lab4.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	stringstream ss(str);
+	for (string word; ss >> word;)
+	{
+		reverse(word.begin(), word.end());
+		cout << word << " ";
+	}
+}
