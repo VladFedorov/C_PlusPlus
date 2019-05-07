117e26c10d783651e5c504d303063b56474f76d7
VUser <nxnxngh@gmail.ocm>
2019-05-07 20:42:15 +0300

    Done

---
+++ Lab3/Lab3/Lab3.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h">
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int i = 0, j, k;
+	string s;
+	cout << "String: ";
+	getline(cin, s);
+
+	while (s[i])
+	{
+		j = k = 0;
+		while (s[j])
+		{
+			if (s[i] == s[j]) k++;
+			j++;
+		}
+		if (k == 1) cout << s[i] << endl;
+		i++;
+	}
+	system("pause");
+	return 0;
+}
